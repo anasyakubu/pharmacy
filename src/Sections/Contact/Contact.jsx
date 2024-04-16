c0693b4087aedb58d87e6900d73c141a96bfdec5
anasyakubu <yakubuanas04@gmail.com>
2024-04-16 11:03:12 +0100

    Contact Section finished

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -7,9 +7,9 @@ const Contact = () => {
     <div className="Contact">
       <div className="p-10 py-14">
         <div className="bg-[#e4e4e4] text-black p-10">
-          <div className="p-3 space-y-2 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+          <div className="p-3 space-y-2 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-0">
             <div className="">
-              <div className="">
+              <div className="lg:py-24 py-5">
                 <h1 className="text-black text-2xl font-semibold">
                   Write to Us
                 </h1>
@@ -23,7 +23,30 @@ const Contact = () => {
                 </div>
               </div>
             </div>
-            <div className=""></div>
+            <div className="">
+              <div className="p-8 bg-white text-black rounded-lg shadow-lg">
+                <div className="">
+                  <label className="text-lg">Your Email :</label>
+                  <input
+                    className="w-full h-full outline-none border border-black mt-3 p-3 rounded-lg"
+                    type="text"
+                  />
+                </div>
+                <div className="mt-5">
+                  <label className="text-lg">Your Email :</label>
+                  <textarea
+                    className="w-full h-full outline-none border border-black mt-3 p-3 rounded-lg"
+                    cols="30"
+                    rows="10"
+                  ></textarea>
+                </div>
+                <div className="mt-5">
+                  <button className="w-full h-full bg-[#268457] text-white p-3 text-lg rounded-lg">
+                    Submit Form
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
